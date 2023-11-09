7280bc562bbb0b49f27795784eddb51ddf499dcd
BrunsO27 <bruno.ortiz_a10@ciencias.unam.mx>
2023-11-09 13:52:38 -0600

    Modelos implementados

--- Zoologico/src/main/java/com/example/Zoologico/model/Alimento.java
+++ Zoologico/src/main/java/com/example/Zoologico/model/Alimento.java
@@ -5,9 +5,80 @@
 package com.example.Zoologico.model;
 
 /**
- *
- * @author bruno
+ * Clase que representa la entidad Alimneto.
+ * @author Valeria Ramírez Vacheron 
+ * @author Eduardo Alfonso Reyes López 
+ * @author Mario Andrés Rosales Peña 
+ * @author Erick Iram García Velasco 
+ * @author Bruno Fernando Ortiz Amaya
  */
 public class Alimento {
+    private String nombre;
+    private String caducidad;
+    private Integer cantidad;
+    private Boolean refrigeracion;
+    private Integer numProveedor;
+    private String tipo;
     
+    public Alimento() {
+    
+    }
+    
+    public Alimento(String nombre, String caducidad, Integer cantidad, 
+            Boolean refrigeracion, Integer numProveedor, String tipo) {
+        this.nombre = nombre;
+        this.caducidad = caducidad;
+        this.cantidad = cantidad;
+        this.refrigeracion = refrigeracion;
+        this.numProveedor = numProveedor;
+        this.tipo = tipo;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }   
+        
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getCaducidad() {
+        return caducidad;
+    }
+    
+    public void setCaducidad(String caducidad) {
+        this.caducidad = caducidad;
+    }
+    
+    public Integer getCantidad() {
+        return cantidad;
+    }
+    
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    public Boolean getRefrigeracion() {
+        return refrigeracion;
+    }
+    
+    public void setRefrigeracion(Boolean refrigeracion) {
+        this.refrigeracion = refrigeracion;
+    }
+    
+    public Integer getNumProveedor() {
+        return numProveedor;
+    }
+    
+    public void setNumProveedor(Integer numProveedor) {
+        this.numProveedor = numProveedor
+    }
+    
+    public String getTipo() {
+        return tipo;
+    }
+    
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
--- Zoologico/src/main/java/com/example/Zoologico/model/Medicina.java
+++ Zoologico/src/main/java/com/example/Zoologico/model/Medicina.java
@@ -5,9 +5,160 @@
 package com.example.Zoologico.model;
 
 /**
- *
- * @author bruno
+ * Clase que repredenta la entidad Medicina
+ * @author Valeria Ramírez Vacheron 
+ * @author Eduardo Alfonso Reyes López 
+ * @author Mario Andrés Rosales Peña 
+ * @author Erick Iram García Velasco 
+ * @author Bruno Fernando Ortiz Amaya
  */
 public class Medicina {
+    private String nombre;
+    private String caducidad;
+    private Integer cantidad;
+    private Boolean refrigeracion;
+    private Integer numProveedor;
+    private String tipo;
+    private String laboratorio;
     
+    /**
+     * Metodo constructor vacio de Medicina 
+     */
+    public Medicina() {
+    
+    }
+    
+    /**
+     * Metodo constructor Medicina
+     * @param nombre - nombre de la medicina
+     * @param caducudad - fecha de caducidad de la medicina
+     * @param cantidad - cantidad de la medicina
+     * @param refrigeracion - refrigeracion de la medicina
+     * @param numProveddor - numero del proveedor de la medicina
+     * @param tipo - tipo de medicina
+     * @param laboratorio - laboratorio de la medicina
+     */
+    public Medicina(String nombre, String caducidad, Integer cantidad, 
+            Boolean refrigeracion, Integer numProveedor, String tipo, 
+            String laboratorio) {
+        this.nombre = nombre;
+        this.caducidad = caducidad;
+        this.cantidad = cantidad;
+        this.refrigeracion = refrigeracion;
+        this.numProveedor = numProveedor;
+        this.tipo = tipo;
+        this.laboratorio = laboratorio;
+    }
+    
+    /**
+     * Metodo que regresa el nombre de la medicina
+     * @return nombre de la medicina
+     */
+    public String getNombre() {
+        return nombre;
+    }   
+        
+    /**
+     * Metodo que asigna un nuevo nombre a la medicina
+     * @param nombre - nuevo nombre 
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    /**
+     * Metodo que regresa la fecha de caducidad de la medicina
+     * @return caducidad de la medicina
+     */
+    public String getCaducidad() {
+        return caducidad;
+    }
+    
+    /**
+     * Metodo que asigna una nueva fecha de caducidad
+     * @param caducidad - nueva fecha de caducidad
+     */
+    public void setCaducidad(String caducidad) {
+        this.caducidad = caducidad;
+    }
+    
+    /**
+     * Metodo que regresa la cantidad de medicina
+     * @return cantidad de medicina
+     */
+    public Integer getCantidad() {
+        return cantidad;
+    }
+    
+    /**
+     * Metodo que asigna una nueva cantidad de la medicina
+     * @param cantidad - nueva cantidad
+     */
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    /**
+     * Metodo que regresa la refrigeracion de la medicina
+     * @return refrigeracion de medicina
+     */
+    public Boolean getRefrigeracion() {
+        return refrigeracion;
+    }
+    
+    /**
+     * Metodo que asigna una nueva refrigeracion de la medicina
+     * @param refrigeracion - nueva refrigeracion
+     */
+    public void setRefrigeracion(Boolean refrigeracion) {
+        this.refrigeracion = refrigeracion;
+    }
+    
+    /**
+     * Metodo que regresa el numero de proveedor de la medicina
+     * @return numero de proveedor de la medicina
+     */
+    public Integer getNumProveedor() {
+        return numProveedor;
+    }
+    
+    /**
+     * Metodo que asigna un nuevo proveedor
+     * @param numProveedor - nuevo numero proveedor
+     */
+    public void setNumProveedor(Integer numProveedor) {
+        this.numProveedor = numProveedor
+    }
+    
+    /**
+     * Metodo que regresa el tipo de la medicina
+     * @return tipo de medicina
+     */
+    public String getTipo() {
+        return tipo;
+    }
+    
+    /**
+     * Metodo que asigna un nuevo tipo a la medicina
+     * @param tipo - nuevo tipo
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    /**
+     * Metodo que regresa el nombre de laboratorio de la medicina
+     * @return laboratorio de la medicina
+     */
+    public String getLaboratorio() {
+        return = laboratorio;
+    }
+    
+    /**
+     * Metodo que asigna un nuevo laboratorio a la medicina
+     * @param laboratorio - nuevo laboratorio
+     */
+    public void setLaboratorio(String laboratorio) {
+        this.laboratorio = laboratorio;
+    }
 }
