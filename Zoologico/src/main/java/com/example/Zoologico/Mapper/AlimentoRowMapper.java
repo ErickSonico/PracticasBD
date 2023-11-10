6e5f51dfa7f3f5662d6333beb1fc1d739cbf2528
ValeVacheron <valeria.rv@ciencias.unam.mx>
2023-11-09 22:00:39 -0600

    Clases Mapper
    Segun yo ya están chidas pero no las he probado jejej

--- Zoologico/src/main/java/com/example/Zoologico/Mapper/AlimentoRowMapper.java
+++ Zoologico/src/main/java/com/example/Zoologico/Mapper/AlimentoRowMapper.java
@@ -4,10 +4,41 @@
  */
 package com.example.Zoologico.Mapper;
 
+import com.example.Zoologico.model.Alimento;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
+
 /**
- *
- * @author bruno
+ * Clase que implementa el metodo para la transformacion de la consulta a un objeto Alimento
+ * 
+ * @author Valeria Ramírez Vacheron 
+ * @author Eduardo Alfonso Reyes López 
+ * @author Mario Andrés Rosales Peña 
+ * @author Erick Iram García Velasco 
+ * @author Bruno Fernando Ortiz Amaya
  */
-public class AlimentoRowMapper {
-    
+public class AlimentoRowMapper  implements RowMapper<Alimento>{
+
+    /**
+     * Metodo que transforma la consulta dada en un objeto Alimento y lo regresa
+     * 
+     * @param rs - resultado de la consulta a transformar
+     * @param rowNum - numero de renglon 
+     * @return Alimento de la consulta
+     * 
+     */
+     @Override
+    public Alimento mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Alimento alim = new Alimento();
+        alim.setIdInsumo(rs.getInt("idInsumo"));
+        alim.setNombre(rs.getString("nombre"));
+        alim.setCaducidad(rs.getString("caducidad"));
+        alim.setCantidad(rs.getInt("cantidad"));
+        alim.setRefrigeracion(rs.getBoolean("refrigeracion"));
+        alim.setTipo(rs.getString("tipo"));  
+        
+        return alim;       
+    }
 }
--- Zoologico/src/main/java/com/example/Zoologico/Mapper/MedicinaRowMapper.java
+++ Zoologico/src/main/java/com/example/Zoologico/Mapper/MedicinaRowMapper.java
@@ -4,10 +4,43 @@
  */
 package com.example.Zoologico.Mapper;
 
+import com.example.Zoologico.model.Medicina;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
+
 /**
- *
- * @author bruno
+ * Clase que implementa el metodo para la transformacion de la consulta a un objeto medicina
+ * 
+ * @author Valeria Ramírez Vacheron 
+ * @author Eduardo Alfonso Reyes López 
+ * @author Mario Andrés Rosales Peña 
+ * @author Erick Iram García Velasco 
+ * @author Bruno Fernando Ortiz Amaya
  */
-public class MedicinaRowMapper {
+public class MedicinaRowMapper  implements RowMapper<Medicina>{
     
+    
+    /**
+     * Metodo que transforma la consulta dada en un objeto medicina y lo regresa
+     * 
+     * @param rs - resultado de la consulta a transformar
+     * @param rowNum - numero de renglon 
+     * @return Medicina de la consulta
+     * 
+     */
+     @Override
+    public Medicina mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Medicina med = new Medicina();
+        med.setIdInsumo(rs.getInt("idInsumo"));
+        med.setNombre(rs.getString("nombre"));
+        med.setCaducidad(rs.getString("caducidad"));
+        med.setCantidad(rs.getInt("cantidad"));
+        med.setRefrigeracion(rs.getBoolean("refrigeracion"));
+        med.setTipo(rs.getString("tipo"));  
+        med.setLaboratorio(rs.getString("laboratorio"));  
+        
+        return med;       
+    }
 }
