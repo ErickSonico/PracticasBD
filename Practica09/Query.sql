1aec34473534c987fec0e563dda244d2abb35f43
BrunsO27 <bruno.ortiz_a10@ciencias.unam.mx>
2023-11-14 16:11:47 -0600

    Consulta de Veterinarios y Cuidadores

--- Practica09/Query.sql
+++ Practica09/Query.sql
@@ -1,7 +1,13 @@
 -- Los veterinarios y cuidadores que comparten el apellido --
+SELECT *
+FROM Veterinario
+WHERE ApellidoPaterno IN (SELECT ApellidoPaterno FROM Cuidador);
 
 -- Los animales herbivoros que pertenezcan al bioma bosque templado --
 SELECT Nombre
 FROM Animal
 WHERE Alimentacion = 'herbivoro' AND
 IDBioma IN (SELECT IDBioma FROM Bioma WHERE Tipo = 'Bosque templado')
+
+-- Biomas que posean almenos 10 animales
+
